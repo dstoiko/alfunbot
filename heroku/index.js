931e9e07875028386b6fe081d76f6a421ea87a2e
dstoiko <davidstoiko@gmail.com>
2016-07-21 11:28:10 +0200

    state prompt test

--- heroku/index.js
+++ heroku/index.js
@@ -131,7 +131,7 @@ function handlePostback(req, res) {
                 this.bot.releaseLock(),
                 this.setState(smoochPayload)
             ]);
-            res.end());
+            res.end();
         break;
 
         default:
