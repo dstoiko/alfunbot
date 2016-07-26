b86ebbf87c3db04bab3d1a8d3926c3f7bfbf9d2f
dstoiko <davidstoiko@gmail.com>
2016-07-26 20:39:54 +0200

    state prompt test

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ module.exports = new Script({
         prompt: (bot) => {
             return bot.say(`Voici les questions qu'on nous pose souvent :
 %[Quand lancez-vous wondor ?](postback:launchDate)
-%[Dans quelles villes ?](postback:placesAvailable)
+%[Dans quelles villes ?](postback:citiesAvailable)
 %[Quels services ?](postback:servicesAvailable)
 %[Qui offre ses services ?](postback:wondorsProfile)
 %[Combien ça coûte ?](postback:howMuch)
@@ -102,7 +102,31 @@ module.exports = new Script({
             .then(() => bot.say(`Besoin de plus d'infos ?
 %[Visiter notre site](http://wondor.co)
 %[Contacter l'équipe](postback:contactRequest)`))
-},
+        },
+        receive: () => 'contactRequest'
+    },
+    launchDate: {
+        prompt: (bot) => bot.say(`Nous sommes en phase de test privé cet été et nous lançons nos services courant septembre. Si vous voulez être parmi les premiers à découvrir wondor, cliquez sur ce lien : %[Me prévenir](http://eepurl.com/b9RLAP)`),
+        receive: () => 'contactRequest'
+    },
+    citiesAvailable: {
+        prompt: (bot) => bot.say(`Nous lançons le service pour Paris et l’Ile-de-France dans un premier temps. Si vous habitez dans une autre région, inscrivez-vous pour être prévenu de notre lancement chez vous : %[Me prévenir](http://eepurl.com/b_jwMv)`),
+        receive: () => 'contactRequest'
+    },
+    servicesAvailable: {
+        prompt: (bot) => bot.say(`Vous pouvez demander n’importe quel service via notre plateforme. Actuellement, nous travaillons avec des bricoleurs, déménageurs et agents de ménage. Mais nous lancerons très prochainement d’autres services. Demandez naturellement un service dont vous auriez besoin, nous ferons au mieux pour l’inclure au plus vite selon la demande ! %[Demander un autre service](postback:otherService)`),
+        receive: () => 'contactRequest'
+    },
+    wondorsProfile: {
+        prompt: (bot) => bot.say(`Tous les wondors sont des prestataires de services qualifiés, que nous avons rencontrés en personne et dont l’identité a été vérifiée.`),
+        receive: () => 'contactRequest'
+    },
+    howMuch: {
+        prompt: (bot) => bot.say(`Nos tarifs sont évalués selon un taux horaire pour le bricolage et le ménage (majoration pour certaines prestations complémentaires), et une grille tarifaire selon le volume à transporter, la distance et les étages pour le déménagement. Dans tous les cas, nous vous communiquons un estimatif dès que vous effectuez une demande via wondor !`),
+        receive: () => 'contactRequest'
+    },
+    workRequest: {
+        prompt: (bot) => bot.say(`Si vous êtes qualifié(e) dans l’un des services que nous proposons, vous pouvez travailler avec nous ! C’est très simple, remplissez l'un de ces formulaires : %[Bricolage](https://goo.gl/forms/M1DkhFAJIcc4cc1H3) %[Déménagement](https://goo.gl/forms/ErBYXvtfQ6qJm9b42) %[Ménage](https://goo.gl/forms/qlcRkjy7d9qVAf7G3)`),
         receive: () => 'contactRequest'
     },
     // If FAQ doesn't answer all the user's questions
