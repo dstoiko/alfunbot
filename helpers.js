128057d4ada7995dd0172d7990f3e00a0abd99e3
MilkshakePanda <iam.superpeluche@gmail.com>
2016-12-18 17:22:48 +0100

    fix syntax error

--- helpers.js
+++ helpers.js
@@ -24,52 +24,10 @@ class SuperSmoochApiBotTwo extends SmoochApiBot {
     }
 }
 
+class SuperStateMachine extends StateMachine {
+
+}
+
 module.exports.SuperSmoochApiBot = SuperSmoochApiBotTwo
+module.exports.SuperStateMachine = SuperStateMachine
 
-// StateMachine.receiveMessage = function(message) {
-
-//     console.log(message);
-
-//     if (message.text === 'lol') {
-//       var last = this.bot.getProp('last');
-//       console.log('last: '+last);
-//       var state = this.getState();
-//       console.log('state: '+state);
-//       if(state === 'human' && Date.now() > last + 1000) {
-//         return Promise.all([
-//             this.bot.releaseLock(),
-//             this.setState('start')
-//         ]);
-//       }
-//     }
-
-//     if (message.text === 'reset') {
-//         return Promise.all([
-//             this.bot.releaseLock(),
-//             this.setState('start')
-//         ]);
-//     }
-
-//     return Promise.all([
-//         this.getState(),
-//         this.bot.acquireLock()
-//     ])
-//         .then((results) => {
-//             const state = results[0];
-//             const lock = results[1];
-
-//             if (!lock) {
-//                 return this.prompt('processing');
-//             }
-
-//             return this.transition(message, state);
-//         })
-//         .then(() => this.bot.releaseLock())
-//         .catch((err) => {
-//             this.bot.releaseLock();
-//             this.prompt('error');
-//             throw err;
-//         });
-// }
-
-// module.exports.StateMachine = StateMachine;
