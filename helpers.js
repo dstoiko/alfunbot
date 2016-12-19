45c1c4b8edf2c7075d713f30849784a1b5554761
dstoiko <davidstoiko@gmail.com>
2016-12-19 12:51:07 +0100

    logging all say messages

--- helpers.js
+++ helpers.js
@@ -29,7 +29,7 @@ class SuperSmoochApiBot extends SmoochApiBot {
             name: this.name,
             avatarUrl: this.avatarUrl
         });
-        console.log('CAROUSEL: ' + message)
+        console.log('CAROUSEL: ' + JSON.stringify(message, null, 2))
         return api.appUsers.sendMessage(this.userId, message)
             .then(data => console.log(data))
             .catch(e => console.log(e.message))
@@ -45,10 +45,8 @@ class SuperSmoochApiBot extends SmoochApiBot {
             name: this.name,
             avatarUrl: this.avatarUrl
         });
-        console.log('TEXT: ' + message);
+        console.log('TEXT: ' + JSON.stringify(message, null, 2));
         return api.appUsers.sendMessage(this.userId, message)
-            .then(data => console.log(data))
-            .catch(e => console.log(e.message))
     }
 }
 
